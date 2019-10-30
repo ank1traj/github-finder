9125bd2b81267849c9fb2e05b750653688312b04
Ankit Raj <ankit.raj928@gmail.com>
2019-10-30 10:18:58 +0530

    Add files via upload

---
+++ github.js
@@ -0,0 +1,19 @@
+class Github {
+  constructor() {
+    this.client_id = 'Iv1.440c89198b635364';
+    this.client_secret = 'f95df9af79f10f0811440273c182dd38259e344f';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile
+    }
+  }
+}
